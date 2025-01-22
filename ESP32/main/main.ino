3cf242010c10daa79abc5cb6e0ba9586f6ed930e
Jhonathan - Pessoal <jhonathan.mke@gmail.com>
2025-01-22 15:49:33 -0300

    adding mqtt topics and wifi connection to the network

--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -7,14 +7,17 @@
 
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <esp_wifi.h>
 
-#define TEMP_TOPIC "esp32/local/uid"
-#define LED_TOPIC "esp32/led"
-const char* ssid = "brisa-1441146";
-const char* password = "xbe6e0nx";
+//#define UID_TOPIC //REMOVER DPS
+String UID_TOPIC;
+String ACCESS_TOPIC;
 
-const char* mqttserver = "broker.hivemq.com";
-const int mqttport = 1883;
+const char* ssid = "brisa-1441146"; //SSID WIFI
+const char* password = "#######"; //WIFI PASSWORD
+
+const char* mqttserver = "broker.hivemq.com"; //BROKER ADDRESS
+const int mqttport = 1883; //BROKER PORT
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -26,7 +29,7 @@ int value = 0;
 #define SS_PIN 5
 #define RST_PIN 2
 
-MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN); //CRIANDO INSTÂNCIA DO mfrc522
 
 MFRC522::MIFARE_Key key; 
 
@@ -34,72 +37,121 @@ byte nuidPICC[4];
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup_wifi()
+byte name0x0[] = { B00100, B01010, B10001, B00100, B01010, B00000, B00100, B00000 };
+
+void scrollText(String message, int row, int delayMs = 300) {
+  int len = message.length();
+  if (len <= 16) {
+    // Se a mensagem couber no display, mostre normalmente
+    lcd.setCursor(0, row);
+    lcd.print(message);
+    return;
+  }
+
+  // Se for maior, faz o scroll
+  for (int i = 0; i < len - 15; i++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(i, i + 16)); // Mostra 16 caracteres por vez
+    delay(delayMs);
+  }
+}
+
+void setup_wifi() //CONFIGURAÇÃO DO WIFI E OBTENÇÃO DE MAC PARA TÓPICO
 {
   delay(10);
-  // Inicia a conexão com a Rede Wi-Fi
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  lcd.clear();
+  lcd.print("Conectando WiFi");
+  lcd.setCursor(0, 1);
+  lcd.print(ssid);
+  delay(500);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-  delay(500);
-  Serial.print(".");
+    delay(500);
+    lcd.print(".");
   }
-  randomSeed(micros());
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  lcd.clear();
+  lcd.print("WiFi Conectado!");
+  delay(2000);
+  lcd.clear();
+  lcd.print("IP: ");
+  lcd.setCursor(0,1);
+  lcd.print(WiFi.localIP());
+
+  uint8_t mac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, mac);
+  char macAddress[18];
+  snprintf(macAddress, sizeof(macAddress), "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  UID_TOPIC = String("esp32/uid/") + macAddress;
+  ACCESS_TOPIC = String("esp32/access/") + macAddress;
+  delay(2000);
+  lcd.clear();
+  lcd.print("MAC:");
+  lcd.setCursor(0, 1);
+  lcd.print(macAddress);
+  delay(2000);
 }
 
-void callback(char* topic, byte* payload, unsigned int length)
+void callback(char* topic, byte* payload, unsigned int length) //FUNÇÃO PARA TRATAMENTO DO PAYLOAD RECEBIDO
 {
   String topicStr;
   String messageStr;
-  topicStr = topic; //Converte o tópico em String
-  for (int i = 0; i < length; i++) { //Converte o payload em String
-  messageStr += (char)payload[i];
+  topicStr = topic;
+
+  for (int i = 0; i < length; i++) {
+    messageStr += (char)payload[i];
   }
+
   Serial.print("Messagem recebida [");
   Serial.print(topicStr);
   Serial.print("] ");
   Serial.println();
-  if(topicStr == LED_TOPIC)
+
+  if(topicStr == ACCESS_TOPIC)
   {
-  // Liga o LED se o tópico LIGADO == "ON"
-  if (messageStr == "ON") {
-  //digitalWrite(LED_PIN, HIGH); // Liga o LED
-  Serial.println("LED Ligado");
+    if (messageStr.isEmpty()) {
+      //digitalWrite(LED_PIN, HIGH); // DESLIGAR LED - NÃO LIBERAR ACESSO
+      Serial.println("Acesso negado!");
+    } else {
+      //digitalWrite(LED_PIN, LOW); // LIGAR LED - ACESSO LIBERADO
+      scrollText(messageStr, 0); //CHAMAR FUNÇÃO DO LCD PARA MOSTRAR TEXTO PASSANDO
+    }
   } else {
-  //digitalWrite(LED_PIN, LOW); //Desliga o LED
-  Serial.println("LED Desligado");
-  }
+    if(topicStr == "esp32/access/all"){
+      lcd.clear();
+      lcd.print(messageStr);
+      lcd.createChar(0, name0x0);
+      lcd.createChar(1, name0x0);
+      lcd.setCursor(15,1);
+      lcd.write(1);
+      lcd.setCursor(0,1);
+      lcd.write(0);
+    }
   }
 }
 
-void reconnect()
+void reconnect() //FUNÇÃO DE RECONEXÃO MQTT
 {
-  // Loop até que a conexão seja bem sucedida
   while (!client.connected()) {
-  Serial.print("Tentando conexão MQTT...");
-  // Cria um ID aleatório para o cliente
-  String clientId = "ESP32 - Sensores";
-  clientId += String(random(0xffff), HEX);
-  // Se conectado
-  if (client.connect(clientId.c_str())) {
-  Serial.println("conectado");
-  // Depois de conectado, publique um anúncio ...
-  //client.publish(TEMP_TOPIC, "0.0");
-  client.publish(LED_TOPIC, "OFF");
-  //... e subscribe.
-  client.subscribe(LED_TOPIC); // <<<<----- mudar aqui
+    lcd.clear();
+    lcd.print("Conectando MQTT");
+    String clientId = "EPS32 - ";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      lcd.clear();
+      lcd.print("MQTT Conectado");
+      delay(2000);
+    
+      client.subscribe("esp32/access/#");
   } else {
-  Serial.print("Falha, rc=");
-  Serial.print(client.state());
-  Serial.println(" Tentando novamente em 5s");
-  delay(5000);
+    lcd.clear();
+    lcd.print("Falha, rc=");
+    lcd.setCursor(0,1);
+    lcd.print(client.state());
+    lcd.clear();
+    lcd.print(" Tentando novamente em 5s");
+    delay(5000);
   }
   }
 }
@@ -113,17 +165,17 @@ void setup() {
     key.keyByte[i] = 0xFF;
   }
 
-  Serial.println(F("This code scan the MIFARE Classsic NUID."));
-  Serial.print(F("Using the following key:"));
-  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
-
   lcd.init();
   lcd.backlight();
+
   lcd.print("Inicializando...");
+  delay(2000);
 
   setup_wifi();
-  client.setServer(mqttserver, 1883); // Publicar
-  client.setCallback(callback); // Receber mensagem
+
+  client.setServer(mqttserver, mqttport); 
+
+  client.setCallback(callback);
 }
 
 void loop() {
@@ -134,54 +186,28 @@ void loop() {
 
   client.loop(); //O processamento das mensagens é assíncrono.
 
-  unsigned long now = millis();
-  if (now - lastMsg > 10000) { //Intervalo de 10s entre as publicacoes
-  lastMsg = now;
-  float temperature;
-  temperature = 10;
-  char s_temp[8];
-  dtostrf(temperature,1,2,s_temp);
-  client.publish(TEMP_TOPIC, s_temp);
-  Serial.print("Temperatura: ");
-  Serial.println(s_temp);
-  }
-
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
   if ( ! rfid.PICC_ReadCardSerial())
     return;
 
-  Serial.print(F("PICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  Serial.println(rfid.PICC_GetTypeName(piccType));
+  lcd.clear();
+  lcd.print("Verificando acesso...");
 
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    Serial.println(F("Your tag is not of type MIFARE Classic."));
-    return;
+  String uidMessage = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    if (rfid.uid.uidByte[i] < 0x10) uidMessage += "0"; // Adicionar 0 para valores menores que 0x10
+    uidMessage += String(rfid.uid.uidByte[i], HEX);
   }
 
-    if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
-    Serial.println(F("A new card has been detected."));
+  client.publish(UID_TOPIC.c_str(), uidMessage.c_str());
 
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
-    }
-   
-    Serial.println(F("The NUID tag is:"));
-    Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-    Serial.print(F("In dec: "));
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
+  lcd.clear();
+  lcd.print("UID Publicado:");
+  lcd.setCursor(0, 1);
+  lcd.print(uidMessage);
+  delay(2000);
+  lcd.clear();
 
   // Halt PICC
   rfid.PICC_HaltA();
@@ -189,22 +215,4 @@ void loop() {
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
 
-
-}
-
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(' ');
-    Serial.print(buffer[i], DEC);
-  }
 }
